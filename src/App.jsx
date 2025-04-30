e2f3e4d8dcad16f1273894542c7b348691faad9f
Andre <andre_ranulfo@hotmail.com>
2025-04-30 18:38:10 -0300

    UseState file created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import UseState02 from './Components/UseState02'
 import UseState03 from './Components/UseState03'
+import UseState04 from './Components/UseState04'
 
 
 function App() {
   return(
     <>
+    <UseState04/>
     <UseState03/> 
     <UseState02/>
     </>
---
+++ src/Components/UseState04.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UseState04 = () => {
+  return (
+   <>
+        <hr />
+        <h3>useState Exercise 4</h3>
+   </>
+  )
+}
+
+export default UseState04
