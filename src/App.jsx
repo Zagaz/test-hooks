524088a86c6091d5029f8ad77e1f2dc86c5f19c3
Andre <andre_ranulfo@hotmail.com>
2025-05-04 20:42:44 -0300

    Comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 import UseState02 from './Components/UseState02'
 import UseState03 from './Components/UseState03'
 import UseState04 from './Components/UseState04'
+// Functional component that receives `value` as a prop
 import UseState05 from './Components/UseState05'
+// Lift The State UP => Pass the data from child to father
 import UseState06Father  from './Components/UseState06Father'
 
 
